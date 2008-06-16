29f9213e7c240c4fa968e031172b468acbd4a390
Stefan Schubert <schubi@suse.de>
2008-06-16 15:48:01 +0000

    added translations

---
+++ src/getText.h
@@ -0,0 +1,43 @@
+/*---------------------------------------------------------------------\
+|                          ____ _   __ __ ___                          |
+|                         |__  / \ / / . \ . \                         |
+|                           / / \ V /|  _/  _/                         |
+|                          / /__ | | | | | |                           |
+|                         /_____||_| |_| |_|                           |
+|                                                                      |
+\---------------------------------------------------------------------*/
+/** \file	getText.h
+ *
+ * Interface to getText.
+ *
+*/
+#ifndef DIALOGSOLVER_GETTEXT_H
+#define DIALOGSOLVER_GETTEXT_H
+
+/** Just tag text for translation. */
+#define N_(MSG) MSG
+
+/** Return translated text. */
+#define _(MSG) ::dialogsolver::dgettext( MSG )
+
+/** Return translated text. */
+#define i18n(MSG) ::dialogsolver::dgettext( MSG )
+
+/** Return translated text (plural form). */
+#define _PL(MSG1,MSG2,N) ::dialogsolver::dngettext( MSG1; MSG2, N )
+
+///////////////////////////////////////////////////////////////////
+namespace dialogsolver
+{ /////////////////////////////////////////////////////////////////
+
+/** Return translated text. */
+    const char * dgettext( const char * msgid );
+
+/** Return translated text (plural form). */
+    const char * dngettext( const char * msgid1, const char * msgid2,
+			    unsigned long n );
+
+    /////////////////////////////////////////////////////////////////
+} // namespace dialogsolver
+
+#endif // DIALOGSOLVER_GETTEXT_H
